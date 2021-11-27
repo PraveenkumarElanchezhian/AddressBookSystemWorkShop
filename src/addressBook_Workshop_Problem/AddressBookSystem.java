4fd6d30902b5ded936b4249bbb59796cb7479425
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-27 20:47:00 +0530

    Edit the contacts in addressBook

--- src/addressBook_Workshop_Problem/AddressBookSystem.java
+++ src/addressBook_Workshop_Problem/AddressBookSystem.java
@@ -1,11 +1,13 @@
 package addressBook_Workshop_Problem;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBookSystem {
 	private static ArrayList<AddressBookContactModel> list = new ArrayList<AddressBookContactModel>();
 
+// AddressBook contact =>	
 	public ArrayList<AddressBookContactModel> AddContactsDetails() {
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Enter the First Name :");
@@ -28,13 +30,87 @@ public class AddressBookSystem {
 		AddressBookContactModel details = new AddressBookContactModel(firstName, lastName, address, city, state, zip,
 				phoneNumber, email);
 		list.add(details);
+		details.displayAddressBook();
 		return list;
 	}
 
-	public static void main(String[] args) {
-		AddressBookSystem myAddressBookSystem = new AddressBookSystem();
-		myAddressBookSystem.AddContactsDetails();
-
+//Edit the existing contacts in AddressBook =>
+	private void editExistingContact() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter firstname to edit contact : ");
+		String name = userInput.nextLine();
+		for (AddressBookContactModel search : list) {
+			if (name.equalsIgnoreCase(search.getFirstName())) {
+				System.out.println("Enter the updated first name : ");
+				String firstName = userInput.next();
+				search.setFirstName(firstName);
+				System.out.println("Enter the updated last name : ");
+				String lastName = userInput.next();
+				search.setLastName(lastName);
+				System.out.println("Enter the updated address : ");
+				String address = userInput.next();
+				search.setAddress(address);
+				System.out.println("Enter the updated city : ");
+				String city = userInput.next();
+				search.setCity(city);
+				System.out.println("Enter the updated state : ");
+				String state = userInput.next();
+				search.setState(state);
+				System.out.println("Enter the updated zipcode : ");
+				int zip = userInput.nextInt();
+				search.setZip(zip);
+				System.out.println("Enter the updated phoneNumber : ");
+				long phoneNumber = userInput.nextInt();
+				search.setPhoneNumber(phoneNumber);
+				System.out.println("Enter the updated emailID : ");
+				String email = userInput.next();
+				search.setEmail(email);
+				search.displayAddressBook();
+			} else {
+				System.out.println("Give proper name");
+			}
+		}
 	}
 
+	public static void main(String[] args) {
+//		AddressBookSystem myAddressBookSystem = new AddressBookSystem();
+//		myAddressBookSystem.AddContactsDetails();
+//		myAddressBookSystem.editExistingContact();
+		Scanner userInput = new Scanner(System.in);
+		HashMap<String, AddressBookContactModel> myaddressBooks = new HashMap<>();
+		AddressBookSystem bookOne = new AddressBookSystem();
+		AddressBookSystem bookTwo = new AddressBookSystem();
+		System.out.println("Choose your Address Book : AddressBook(1)/ AddressBook(2)");
+		int chooseAddressBook = userInput.nextInt();
+		System.out.println("Choose your Address Book : Add/Edit");
+		while (chooseAddressBook >= 1) {
+			System.out.println("Enter Your Choice : Add Contacts (1)/Edit Contacts (2)");
+			int choice = userInput.nextInt();
+			switch (chooseAddressBook) {
+			case 1:
+				if (choice == 1) {
+					bookOne.AddContactsDetails();
+				} else if (choice == 2) {
+					bookOne.editExistingContact();
+				}
+				break;
+			case 2:
+				if (choice == 1) {
+					bookTwo.AddContactsDetails();
+				} else if (choice == 2) {
+					bookTwo.editExistingContact();
+				}
+				break;
+
+			default:
+				System.out.println("Give proper input");
+				break;
+			}
+			System.out.println("1) AddressBook 1");
+			System.out.println("2) AddressBook 2");
+			System.out.println("3) AddressBook 3");
+			System.out.println("0) Exit");
+		}
+
+	}
 }
